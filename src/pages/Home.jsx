d233bf553254058f094fa84ce24fbfdedb4d130f
פרויקט גמר <shumshum4055@gmail.com>
2025-12-29 21:01:41 +0000

    Manual code change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,5 +2,14 @@ import React from 'react';
 import ShiftCalendar from '@/components/calendar/ShiftCalendar';
 
 export default function Home() {
-  return <ShiftCalendar />;
+  return (
+    <>
+      <ShiftCalendar />
+      
+      {/* זכויות יוצרים - קבוע למטה בצד שמאל */}
+      <div className="fixed bottom-2 left-4 text-[10px] text-gray-400 font-medium select-none z-50 opacity-70 hover:opacity-100 transition-opacity" dir="rtl">
+        © פותח ע״י ענף דיגיטל {new Date().getFullYear()}
+      </div>
+    </>
+  );
 }
